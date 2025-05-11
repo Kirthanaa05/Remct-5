cbc9adc17dbaf1b3c34b2df58dedcf5cf5e4e868
Kirthanaa05 <kirthanaa337@gmail.com>
2025-05-11 20:09:50 +0530

    Updated the contacts compt

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -4,6 +4,7 @@ const Contact = () => {
   return (
     <div className='contact'>
       <h1>Contact Us</h1>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Non incidunt ducimus harum nostrum atque inventore tempora eveniet a molestias exercitationem beatae, doloribus, laudantium placeat culpa, quis aliquam vitae ullam repellendus.</p>
     </div>
   );
 };
